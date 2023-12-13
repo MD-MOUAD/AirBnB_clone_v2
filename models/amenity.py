729c94253894b918b3d6e47506abab5087e9150b
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-13 22:15:57 +0100

    update classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """ documment doc """
-    __tablename__ = 'amenities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity',
-                                       back_populates="amenities")
-    else:
-        name = ""
+class Amenity(BaseModel):
+    name = ""
