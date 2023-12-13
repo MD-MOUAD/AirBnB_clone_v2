fd5cb037330ab14cef7aa18b5933ab43922f4886
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-13 01:14:21 +0100

    fix console

--- models/base_model.py
+++ models/base_model.py
@@ -21,10 +21,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            # from models import storage
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -43,7 +44,6 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
