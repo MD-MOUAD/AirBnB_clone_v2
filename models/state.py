55d1926a95fee149a39ba3409d6f2d7f947be236
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-13 20:03:58 +0100

    db storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all, delete", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id
+            equals to the current State.id"""
+            from models import storage
+            related_cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
