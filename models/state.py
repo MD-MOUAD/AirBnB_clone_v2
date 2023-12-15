df231ae2dd254be0b44e314e8bf0342c5123711e
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-15 17:10:23 +0100

    classes

--- models/state.py
+++ models/state.py
@@ -18,11 +18,6 @@ class State(BaseModel, Base):
         cities = relationship('City', cascade="all,delete", backref="state")
     else:
         name = ""
-        # DONE: for FileStorage: getter attribute cities that
-        # returns the list of City instances with state_id equals
-        # to the current State.id => It will be the FileStorage
-        # relationship between State and City
-
         @property
         def cities(self):
             """getter docuemnt"""
