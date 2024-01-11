255419a8760b3c5453380b4ed36b8992753b20cf
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-01-11 13:14:42 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,27 @@
 #!/usr/bin/env bash
-#Bash script that sets up web servers for the deployment of web_static
+#Set up web servers for the deployment of web_static
 
-#Install nginx.
+#Install Nginx if it not already installed
 sudo apt update
-sudo apt install -y nginx
+sudo apt install nginx -y
 
 #Create default nginx folder for the pages.
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
-#Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-sudo touch /data/web_static/releases/test/index.html
+#Create a fake HTML file to test Nginx configuration
+echo "Hello Alx!" > /data/web_static/releases/test/index.html
 
 #Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
-# If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+#If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 sudo rm /data/web_static/current
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-#Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
-# This should be recursive; everything inside should be created/owned by this user/group.
+#Give ownership of the /data/ folder to the ubuntu user AND group ( should be recursive).
 sudo chown -R ubuntu:ubuntu /data/
 
-#Overwite file content.
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
-#Create an endpoint to index.html.
-sudo sed -i "s/server_name _;/&\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t\tindex index.html;\n\t}\n/" /etc/nginx/sites-enabled/default
+#Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static.
+sudo sed -i "s/server_name _;/&\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}\n/" /etc/nginx/sites-enabled/default
 
 #Restart nginx server.
 sudo service nginx restart
