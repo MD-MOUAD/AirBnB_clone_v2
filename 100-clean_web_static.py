79f201dbd96fbb0e9219067da5b37db379e7c95b
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-01-15 17:36:37 +0100

    try

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import task, local, env, put, run, runs_once
 from datetime import datetime
 import os
 
-env.hosts = ['18.207.1.87', '52.206.189.175']
+env.hosts = ['3.94.185.86', '35.153.50.176']
 
 
 @runs_once
