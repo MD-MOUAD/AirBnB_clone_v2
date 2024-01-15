bfee96656fe9ed23ae38d453a89653e21d62c181
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-01-15 16:26:15 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,17 +6,41 @@ sudo apt update
 sudo apt install nginx -y
 
 #Create default nginx folder for the pages.
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/  #will contain current (symbolic link to test/)
+
+#Server block
+ServerBlock="server {
+    listen 80;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    add_header X-Served-By \$HOSTNAME;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+
+}"
+#Nginx config file path
+Config="/etc/nginx/sites-available/default"
 
 #Create a fake HTML file to test Nginx configuration
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+Fake="<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>"
+echo "$Fake" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 #Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
 #If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
@@ -27,7 +51,7 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 #Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static.
-sudo sed -i "s/server_name _;/&\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}\n/" /etc/nginx/sites-enabled/default
+echo "$ServerBlock" | sudo tee "$Config" > /dev/null
 
 #Restart nginx server
 sudo service nginx restart
